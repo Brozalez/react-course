93ad0180330e0b0e664521b0c9649c330921382e
Breno <breno.bueno@festo.com>
2022-09-08 15:20:15 -0300

    added more stuff

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,12 @@
 import Button from "../buttom/buttom.component";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-itens" />
+      [].map(item => <CartItem cartItem={item} />)
       <Button>Go to checkout</Button>
     </div>
   );
