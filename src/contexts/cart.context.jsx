852fa2d5bdd7adc8d46c99f388cbd33d142c3df0
Breno <brenodbueno@gmail.com>
2022-09-08 23:15:57 -0300

    added other stuff

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,17 @@
 import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  )
+
+  if(existingCartItem){
+    return cartItems.map((cartItem) => cartItem.id === productToAdd.id ?
+    {...cartItem, quantity: cartItem.quantity + 1 }
+    : cartItem)
+  }
+
+
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
